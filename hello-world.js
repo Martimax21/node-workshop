48f6f539b8c27b422ccc55bdcdefe0fc06319827
Maxime Martin <maxime.martin123@gmail.com>
2016-07-11 17:50:59 +0000

    FirstProgram

---
+++ hello-world.js
@@ -0,0 +1,12 @@
+// First program
+// Create a file called hello-world.js . In it, write a simple node program that outputs "Hello World!" to the console.
+// Add an instruction to your program that will output "Hello World Again!!" 10 seconds after the program was run.
+// Save, commit and push.
+
+function hello(){
+    setTimeout(function() {
+        console.log("Hello World!")
+    }, 10000);
+}
+
+hello();
