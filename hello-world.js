1cb5772eef585637d8df9ab34ef2a6b64e34d1af
Maxime Martin <maxime.martin123@gmail.com>
2016-07-11 18:07:18 +0000

    wildInterval

--- hello-world.js
+++ hello-world.js
@@ -3,9 +3,50 @@
 // Add an instruction to your program that will output "Hello World Again!!" 10 seconds after the program was run.
 // Save, commit and push.
 
+
+// function hello(){
+//     setTimeout(function() {
+//         console.log("Hello World!")
+//     }, 1000);
+// }
+//hello();
+
+// A wild interval has appeared!
+// If you never tried setInterval, give it a try first. It works the same way as setTimeout, takes a callback 
+// function and a time in milliseconds. But instead of calling your callback once, it calls it once every x milliseconds
+// . In this exercise, you’ll have to mimic what setInterval is doing but only with setTimeout!
+// Your exercise: Write a piece of code using setTimeout that prints “Hello World!” every 10 seconds forever.
+// One idea that may come to you is to do something like this:
+// setTimeout(function() {
+//   console.log('Hello World!');
+//   setTimeout(function() {
+//     console.log('Hello World!');
+//     // when does this stop??
+//   }, 1000);
+// }, 1000);
+// This won’t work, because you’d have to write an infinite amount of code.
+// Another idea you might have is to do the following:
+// while (true) {
+//   setTimeout(function() {
+//     console.log('Hello World!');
+//   }, 1000);
+// }
+// Up to you to figure out why this does not work ;) Ask us if you can’t figure it out. Then, find a real solution.
+
+// Two BIG hints: the solution involves recursion. You will have to write a function.
+
+// var test = setInterval(function() {
+//     console.log("Hello World!")
+// }, 1000);
+
+// setTimeout(function() {
+//     clearInterval(test)
+// }, 3000)
+
 function hello(){
     setTimeout(function() {
         console.log("Hello World!")
+        hello();
     }, 10000);
 }
 
